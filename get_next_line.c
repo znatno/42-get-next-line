3ad24607854cca0e16db1465def469bb2ff7b0b8
Ivan <ivanbohuna@gmail.com>
2019-03-03 19:07:30 +0200

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,28 @@
 /*   By: ibohun <ibohun@student.unit.ua>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 13:54:34 by ibohun            #+#    #+#             */
-/*   Updated: 2019/03/03 16:41:54 by ibohun           ###   ########.fr       */
+/*   Updated: 2019/03/03 16:56:20 by ibohun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	create_list(const char **line)
+static t_gnl	create_list(const char **line)
 {
 
 }
 
-int	fd_count()
+int				fd_count()
 {
 
 }
 
-int	check_list()
+int				check_list()
 {
 
 }
 
-int	get_next_line(const int fd, const char **line)
+int				get_next_line(const int fd, const char **line)
 {
 	static t_gnl new;
 
@@ -36,5 +36,5 @@ int	get_next_line(const int fd, const char **line)
 	new = create_list(&line);
 
 
-
+	return (check_list(fd, line, new))
 }
