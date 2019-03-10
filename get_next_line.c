2ef6002e437e530e7745b57e54fead6635be6f2f
Ivan <ivanbohuna@gmail.com>
2019-03-10 15:33:49 +0200

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ibohun <ibohun@student.unit.ua>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 13:54:34 by ibohun            #+#    #+#             */
-/*   Updated: 2019/03/10 15:23:03 by ibohun           ###   ########.fr       */
+/*   Updated: 2019/03/10 15:33:41 by ibohun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@ static t_gnl	*create_list(const int fd)
 {
 		t_gnl	*new;
 
-		if (new = (t_gnl*)malloc(sizeof(*new)))
-		{
-			new->fd = fd;
-			new->buff = ft_strnew(0);
-			new->tmp = NULL;
-			new->next = NULL;
-			return (new);
-		}
-		return (NULL);
+		if (!(new = (t_gnl*)malloc(sizeof(*new))))
+				return (NULL);
+		new->fd = fd;
+		new->buff = ft_strnew(0);
+		new->tmp = NULL;
+		new->next = NULL;
+		return (new);
 }
 
 /*
@@ -48,14 +46,21 @@ int						get_next_line(const int fd, char **line)
 	//char			**str;
 	int				ret;
 
-	if (fd < 0 || !line)
+
+	// if (/* умова що вже нема що віддавати*/)
+	// {
+	// 	/* очищення пам'яті */
+	// 	return (0);
+	// }
+
+	if (fd < 0 || !line || BUF_SIZE <= 0)
 		return (-1);
-	if (!*saved)
-	saved = create_list(fd);
+	if (!saved)
+		saved = create_list(fd);
 	ret = read(fd, buff, BUF_SIZE);
 	buff[ret] = '\0';
 	saved->buff = buff;
 	printf("%s\n", saved->buff);
-	free(saved);
-	return (0);
+	//free(saved);
+	return (1);
 }
